e81c0070daac589147be203d5773050e38aaa898
evgeni83 <evgeni.daniluk@yandex.ru>
2019-07-07 21:27:48 +0300

    Form action

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-	$('.fly-item').removeClass('fly-item_anim');
+$(document).ready(function () {
 	var $page = $('html, body');
-	$('a[href*="#"]').click(function() {
+	$('a[href*="#"]').click(function () {
 		$page.animate({
 			scrollTop: $($.attr(this, 'href')).offset().top
 		}, 400);
@@ -10,6 +9,7 @@ $(document).ready(function(){
 	$('.multiple-items').slick({
 		dots: true,
 		infinite: true,
+		//autoplay: true,
 		speed: 300,
 		slidesToShow: 3,
 		slidesToScroll: 3,
@@ -17,45 +17,66 @@ $(document).ready(function(){
 			{
 				breakpoint: 1030,
 				settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2,
-				infinite: true,
-				dots: true
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true
 				}
-		    },
-		    {
-			    breakpoint: 700,
-			    settings: {
-			    slidesToShow: 1,
-			    slidesToScroll: 1
-			    }
-		    },
-		    // You can unslick at a given breakpoint now by adding:
-		    // settings: "unslick"
-		    // instead of a settings object
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
 		],
 		nextArrow: '<button type="button" class="button right-arrow__portfolio"></button>',
 		prevArrow: '<button type="button" class="button left-arrow__portfolio"></button>',
 	});
-	$('.burger-menu').click(function(){
+
+	$('.burger-menu').click(function () {
 		$(this).toggleClass('burger-menu_active');
 		$('.header__mobile-links').toggleClass('header__mobile-links_active');
 	});
-	$('.nav-link_mobile').click(function(){
+
+	$('.nav-link_mobile').click(function () {
 		$('.burger-menu').removeClass('burger-menu_active');
 		$('.header__mobile-links').removeClass('header__mobile-links_active');
 	});
+
+	$('.fly-item').removeClass('fly-item_anim');
+
 	$('.show_popup').click(function () {
-		$('.popup-container').fadeIn(500);
+		$('.form-container').fadeIn(500);
 		$('.popup-container').css('display', 'flex');
 		$('body').css('overflow', 'hidden');
 		return false;
 	});
+
+	$('.popup__form').submit(function () { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).parent().parent().parent().fadeOut(100);
+			$('.thanks-cotainer').fadeIn();
+			$('.popup__form').find('input').val('');
+			$('.popup__form').trigger('reset');
+		});
+		return false;
+	});
+
 	$('.close-button').click(function () {
 		$(this).parent().parent().fadeOut(100);
 		$('body').css('overflow', 'auto');
 		return false;
 	});
+
 	$('.popup-container').click(function (event) {
 		if (event.target == this) {
 			$(this).fadeOut(100);
@@ -63,6 +84,7 @@ $(document).ready(function(){
 			return false;
 		}
 	});
+
 	jQuery(function ($) {
 		$("#phone").mask("+7(999) 999-99-99");
 	});
